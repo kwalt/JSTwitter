e3f60343938d7618dc78b859c6e0ea9974b7f193
Kurt Walters <walters.kurt@gmail.com>
2012-04-02 22:43:50 -0400

    lots of bullshit in here

--- jstwitter.rb
+++ jstwitter.rb
@@ -13,9 +13,15 @@ class JSTwit
 		command = ""
 		while command != "q"
 			printf("enter a command: ")
-			command = gets.chomp
+			input = gets.chomp
+			sections = input.split(" ")
+			command = sections[0]
 			case command
 				when 'q' then puts "Goodbye!"
+				when 't' then twitterize(sections[1..-1].join(" "))
+					puts "Message posted."
+				when 'dm' then dm(sections[1], sections[2..-1].join(" "))
+				when 'elt' then everyones_last_tweet
 				else
 					puts "Sorry bro, I don't know how to do #{command}"
 				end
@@ -31,9 +37,44 @@ class JSTwit
 		end
 	end
 
+	def dm(recipient, message)
+		puts "Trying to send #{recipient} this message:"
+		puts message
+		screen_name = @client.followers.collect{|follower| follower.screen_name}
+		puts screen_name
+		#Need to go back and do something to make case irrelevant
+		if screen_name.include?(recipient) == true
+			twitterize("d #{recipient} #{message}")
+			puts "Direct message sent."
+		else
+			puts "Sorry, you can only DM people who are following you."
+		end
+	end
+
+	def followers_list
+		screen_names = []
+		# Can you also do screen_names = Array.new ?
+		your_followers = @client.followers
+		your_followers.each do |follower|
+			screen_names << followers["screen_names"]
+		#WHAT THE FUCK???
+		end
+	end
+
+	def spam_my_followers
+		#put shit here
+	end
+
+	def everyones_last_tweet
+		friends = @client.friends
+		friends.each do |friend|
+			puts friend.screen_name
+			last_tweets << friend.status.text
+			puts ""
+		end
+	end
+
 end
-
-
 
 #Script
 dude = JSTwit.new
