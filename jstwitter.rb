5cc8ad5024cfa1d36ef90a9fc8bfc581b54727d4
Kurt Walters <walters.kurt@gmail.com>
2012-04-08 00:31:17 -0400

    Done with JSTwitter

--- jstwitter.rb
+++ jstwitter.rb
@@ -1,4 +1,6 @@
 require 'jumpstart_auth'
+require 'bitly'
+require 'klout'
 
 class JSTwit
 	attr_reader :client
@@ -6,6 +8,7 @@ class JSTwit
 	def initialize
 		puts "Initializing"
 		@client = JumpstartAuth.twitter
+		@k = Klout::API.new('6f2zva63qwtan3hgwvesa7b8')
 	end
 
 	def startup
@@ -22,6 +25,10 @@ class JSTwit
 					puts "Message posted."
 				when 'dm' then dm(sections[1], sections[2..-1].join(" "))
 				when 'elt' then everyones_last_tweet
+				when 'spam' then spam_my_followers(sections[1..-1].join(" "))
+				when 's' then make_short(sections[1..-1].join(" "))
+				when 'turl' then twitterize(sections[1..-2].join(" ") + " "	+ make_short(sections[-1]))
+				when 'k' then find_klout
 				else
 					puts "Sorry bro, I don't know how to do #{command}"
 				end
@@ -40,10 +47,10 @@ class JSTwit
 	def dm(recipient, message)
 		puts "Trying to send #{recipient} this message:"
 		puts message
-		screen_name = @client.followers.collect{|follower| follower.screen_name}
-		puts screen_name
+		screen_names = @client.followers.collect{|follower| follower.screen_name}
+		puts screen_names
 		#Need to go back and do something to make case irrelevant
-		if screen_name.include?(recipient) == true
+		if screen_names.include?(recipient) == true
 			twitterize("d #{recipient} #{message}")
 			puts "Direct message sent."
 		else
@@ -56,21 +63,59 @@ class JSTwit
 		# Can you also do screen_names = Array.new ?
 		your_followers = @client.followers
 		your_followers.each do |follower|
-			screen_names << followers["screen_names"]
-		#WHAT THE FUCK???
+			screen_names << follower[:screen_name] #Why is this symbol v string?
+		end
+		return screen_names
+	end
+
+	def spam_my_followers(message)
+		list = followers_list
+		list.each do |friend|
+			dm(friend, message)
 		end
 	end
 
-	def spam_my_followers
-		#put shit here
+	def everyones_last_tweet
+		friends = @client.friends.sort_by{|friend| friend.screen_name.downcase}
+		#puts friends
+		friends.each do |friend|
+			timestamp = friend.status.created_at.strftime("%A, %b, %d")
+			puts "#{friend.screen_name} said on #{timestamp}:"
+
+			puts friend.status.text
+			puts ""
+		end
 	end
 
-	def everyones_last_tweet
-		friends = @client.friends
-		friends.each do |friend|
-			puts friend.screen_name
-			last_tweets << friend.status.text
-			puts ""
+	def make_short(url)
+		puts "Shortening this URL: #{url}"
+		input_test = url.split(" ")
+		if input_test[1].nil?
+			Bitly.use_api_version_3
+
+			connex = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+			shortened = connex.shorten(url).short_url
+			puts shortened
+			return shortened
+		else
+			puts "Sorry, it appears you entered something more than just a URL. 
+			Please check your input and try again."
+		end
+	end
+
+	def find_klout
+		puts "Friends' klout:"
+		friend_list = @client.friends.collect{ |friend| friend.screen_name}
+		friend_list.each do |friend|
+			puts friend
+			puts @k.klout(friend)["users"][0]["kscore"]
+		end
+
+		puts ""
+		puts "Followers' klout:"
+		followers_list.each do |follower|
+			puts follower
+			puts @k.klout(follower)["users"][0]["kscore"]
 		end
 	end
 
@@ -80,3 +125,5 @@ end
 dude = JSTwit.new
 #dude.twitterize("JSTwit Init")
 dude.startup
+#dude.followers_list
+#dude.find_klout
